a099ce68f0c7326e83d349c7b024e811287ea666
Alex <virdata@yandex.ru>
2022-03-29 23:05:33 +0800

    Решение задачи 3

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -10,6 +10,13 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите фразу: ");
+            string text = Console.ReadLine();
+            char[] arraytext = text.ToCharArray();
+            Array.Reverse(arraytext);
+            Console.Write("Результат: ");
+            Console.WriteLine(arraytext);
+            
         }
     }
 }
